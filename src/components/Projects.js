2a1c2a0c6db7747e009f785fd1faaa13281fdf85
JakubowskiA <arieljakubowski@gmail.com>
2019-09-24 17:48:27 -0400

    Added basic outline

---
+++ src/components/Projects.js
@@ -0,0 +1,50 @@
+import React, { Fragment } from 'react';
+
+function Projects() {
+  return (
+    <Fragment>
+    <div className="project-container">
+    <h3></h3>
+    <img src=""/>
+    <h4></h4>
+    <p></p>
+    <ul>
+        <li></li>
+    </ul>
+    </div>
+
+    <div className="project-container">
+    <h3></h3>
+    <img src=""/>
+    <h4></h4>
+    <p></p>
+    <ul>
+        <li></li>
+    </ul>
+    </div>
+
+    <div className="project-container">
+    <h3></h3>
+    <img src=""/>
+    <h4></h4>
+    <p></p>
+    <ul>
+        <li></li>
+    </ul>
+    </div>
+
+    <div className="project-container">
+    <h3></h3>
+    <img src=""/>
+    <h4></h4>
+    <p></p>
+    <ul>
+        <li></li>
+    </ul>
+    </div>
+    
+    </Fragment>
+  );
+}
+
+export default Projects;
