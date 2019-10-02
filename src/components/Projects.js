f998e1350979d972ebbd9d825e3e5833f1444c2c
JakubowskiA <arieljakubowski@gmail.com>
2019-10-01 23:36:15 -0400

    Added project info

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,7 +11,9 @@ function Projects() {
                 <figure>
                 <img src="projectImgs/serene.png" className="project-img"/>
                 <figcaption>
-                    <h1>My hover Overlay</h1>
+                    <h3>SERENE</h3>
+                    <h4>A guided online journal to help you evaluate and manage stress in a productive and healthy way</h4>
+                    <h4>Technologies used: </h4>
                     <a href="#" class="button">Learn More</a>
                 </figcaption>
                 </figure>
@@ -20,10 +22,11 @@ function Projects() {
             <figure>
                 <img src="projectImgs/readme.png" className="project-img"/>
                 <figcaption>
-                    <h1>My hover Overlay</h1>
+                    <h3>READ ME</h3>
+                    <h4>Find your next read and track your books with Read Me</h4>
                     <a href="#" class="button">Learn More</a>
                 </figcaption>
-                </figure>
+            </figure>
             </div>
         </div>
         <div className="project-img-group">
@@ -31,16 +34,18 @@ function Projects() {
             <figure>
                 <img src="projectImgs/wordle.png" className="project-img"/>
                 <figcaption>
-                    <h1>My hover Overlay</h1>
+                    <h3>WORDLE</h3>
+                    <h4>A fun word game that lets users test their vocabulary and typing skills</h4>
                     <a href="#" class="button">Learn More</a>
                 </figcaption>
-                </figure>
+            </figure>
             </div>
             <div className="project">
             <figure>
                 <img src="projectImgs/bpf.png" className="project-img"/>
                 <figcaption>
-                    <h1>My hover Overlay</h1>
+                    <h3>BEST PAW FORWARD</h3>
+                    <h4>The easiest way to find training for your pet and manage training sessions</h4>
                     <a href="#" class="button">Learn More</a>
                 </figcaption>
                 </figure>
@@ -50,9 +55,7 @@ function Projects() {
 
     {/* <div className="project-container">
     <h3>SERENE</h3>
-    <img src=""/>
     <h4>A GUIDED ONLINE JOURNAL TO HELP YOU EVALUATE AND MANAGE STRESS IN A PRODUCTIVE AND HEALTHY WAY</h4>
-    <p></p>
     <ul>
         <li>Implemented customizable user interface employing React, custom CSS, Chart.js, Moment.js and Semantic UI</li>
         <li>Utilized React Router to implement client side routing and allow users to easily and intuitively navigate webpage</li>
@@ -63,9 +66,7 @@ function Projects() {
 
     <div className="project-container">
     <h3>READ ME</h3>
-    <img src=""/>
     <h4>FIND YOUR NEXT READ AND TRACK YOUR BOOKS WITH READ ME</h4>
-    <p></p>
     <ul>
         <li>Utilized Google Books API to pull and parse data on books, built backend with Rails, created user database with Postgres</li>
         <li>Implemented user interface employing React, Semantic UI, and custom CSS</li>
@@ -76,9 +77,7 @@ function Projects() {
 
     <div className="project-container">
     <h3>WORDLE</h3>
-    <img src=""/>
     <h4>A FUN WORD GAME THAT LETS USERS TEST THEIR VOCABULARY AND TYPING SKILLS</h4>
-    <p></p>
     <ul>
         <li>Built backend with Ruby on Rails, stored user data with Postgres</li>
         <li>Designed user interface with HTML, JavaScript, custom CSS, and Particle.js</li>
@@ -88,9 +87,7 @@ function Projects() {
 
     <div className="project-container">
     <h3>BEST PAW FORWARD</h3>
-    <img src=""/>
     <h4>THE EASIEST WAY TO FIND TRAINING FOR YOUR PET AND MANAGE TRAINING SESSIONS</h4>
-    <p></p>
     <ul>
         <li>Built full stack functionality with Ruby on Rails using MVC structure </li>
         <li>Created database schema and configured ActiveRecord associations with Rails and SQLite3</li>
