f096d97da0b39ea21cbdd366c578649e5cad2e65
JakubowskiA <arieljakubowski@gmail.com>
2019-10-04 14:57:41 -0400

    Added fonts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,24 +6,23 @@ function Contact() {
         <h2>Contact</h2>
         <div className="contact-group">
             <a href="https://www.linkedin.com/in/ariel-jakubowski/">
-                <div className="contact-icon">in</div>
+                <div className="contact-icon hvr-grow">in</div>
             </a>
             <a href="https://medium.com/@arieljakubowski">
-                <div className="contact-icon">M</div>
+                <div className="contact-icon hvr-grow">M</div>
             </a>
             <a href="Resume.pdf">
-                <div className="contact-icon">
+                <div className="contact-icon hvr-grow">
                     <img className="contact-icon-img" src="https://icon-library.net/images/document-icon-image/document-icon-image-27.jpg"/>
-                </div>
-                
+                </div>           
             </a>
             <a href="https://github.com/JakubowskiA">
-                <div className="contact-icon">
+                <div className="contact-icon hvr-grow">
                     <img className="contact-icon-img" src="https://icon-library.net/images/github-svg-icon/github-svg-icon-3.jpg"/>
                 </div>
             </a>
             <a class="mailto" href="mailto:arieljakubowski@gmail.com">
-                <div className="contact-icon">
+                <div className="contact-icon hvr-grow">
                     <img className="contact-icon-img" src="https://image.flaticon.com/icons/svg/33/33700.svg"/>
                 </div>
             </a>
