54430ac7b59ac62526180e74e8814a2aada1e6e3
JakubowskiA <arieljakubowski@gmail.com>
2019-10-22 15:49:00 -0400

    made contact section more responsive and updated favicon

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,16 +26,22 @@ function Contact() {
                 <div className="contact-icon hvr-show">Resume</div>           
             </a>
             </div>
+            <div className="contact contact-div">
             <a href="https://github.com/JakubowskiA">
-                <div className="contact-icon hvr-grow">
+                <div className="contact-icon hvr-grow hvr-hide">
                     <img className="contact-icon-img" src="https://icon-library.net/images/github-svg-icon/github-svg-icon-3.jpg"/>
                 </div>
+                <div className="contact-icon hvr-show">Github</div>
             </a>
+            </div>
+            <div className="contact contact-div">
             <a className="mailto" href="mailto:arieljakubowski@gmail.com">
-                <div className="contact-icon hvr-grow">
+                <div className="contact-icon hvr-grow hvr-hide">
                     <img className="contact-icon-img" src="https://image.flaticon.com/icons/svg/33/33700.svg"/>
                 </div>
+                <div className="contact-icon hvr-show">Email</div>
             </a>
+            </div>
         </div>
     </div>
   );
