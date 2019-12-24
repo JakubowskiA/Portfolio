6d44b357cf7def87112be169792dfa0169282ba3
JakubowskiA <arieljakubowski@gmail.com>
2019-12-24 10:56:35 -0500

    Added blog post

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -54,6 +54,28 @@ function Blog() {
                     <p className="publisher">Published independently </p>
                     <a href="https://medium.com/@arieljakubowski/a-quick-guide-to-css-df5de9f9a178"><button>See blog post</button></a>
                 </div>
+            <div className="blog-content-container">
+                <div className="blog">
+                    <img className="blog-pic" src ="https://miro.medium.com/max/980/1*dgEOd9EmJ-wvmjhZ7fTFNw.png"/>
+                    <hr/>
+                    <p>Popular JavaScript Array Methods</p>
+                    <p className="publisher">Published by Better Programming</p>
+                    <a href="https://medium.com/better-programming/popular-javascript-array-methods-ca4d24c9243f"><button>See blog post</button></a>
+                </div>
+                {/* <div className="blog">
+                    <img className="blog-pic" src ="https://miro.medium.com/max/591/1*HoDhjtqIPFPCZUwrEl_WUA.png"/>
+                    <hr/>
+                    <p>Clearing Up the Cloud</p>
+                    <p className="publisher">Published independently </p>
+                    <a href="https://medium.com/@arieljakubowski/clearing-up-the-cloud-9f8aacc609b5"><button>See blog post</button></a>
+                </div>
+                <div className="blog">
+                    <img className="blog-pic" src ="https://cdn.lynda.com/course/504266/504266-636924000532723800-16x9.jpg"/>
+                    <hr/>
+                    <p>A Quick Guide to CSS</p>
+                    <p className="publisher">Published independently </p>
+                    <a href="https://medium.com/@arieljakubowski/a-quick-guide-to-css-df5de9f9a178"><button>See blog post</button></a>
+                </div> */}
             </div>
             <br/><br/>
         </div>
