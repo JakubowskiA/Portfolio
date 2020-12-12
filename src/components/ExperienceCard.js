b93b8b92bbe1cd724644d99ebbe6b524d66b27fa
Ariel Jakubowski <arieljakubowski@gmail.com>
2020-12-12 10:40:17 -0500

    Experience card updates

--- src/components/ExperienceCard.js
+++ src/components/ExperienceCard.js
@@ -6,12 +6,12 @@ export const ExperienceCard = (props) => {
         <div>
         <img className="project-card-image" src={props.companyIcon} />
         <p id="projectName">{props.companyName}</p>
-        <p id="projectName">{props.dateRange}</p>
+        <p id="projectName">{props.title}</p>
         </div>
-      
-      <p id="projectName">{props.name}</p>
       <p>{props.description}</p>
-      <p>Technologies used: {props.technologiesUsed}</p>
+      <div>
+      <p>{props.dateRange}</p>
+      </div>
     </div>
   );
 };
--- src/components/ExperienceData.js
+++ src/components/ExperienceData.js
@@ -1,6 +1,14 @@
-export const projectsData = [
+export const experienceData = [
+    {
+      companyName="Wayfair",
+      title="",
+      companyIcon="",
+      dateRange="February 2020 - present",
+      description=""
+    },
     {
       companyName="",
+      title="",
       companyIcon="",
       dateRange="",
       description=""
