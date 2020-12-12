97260d93c49b3a473986313a9266721ba55c0ef9
Ariel Jakubowski <arieljakubowski@gmail.com>
2020-12-12 11:31:47 -0500

    Experience data

--- src/components/ExperienceData.js
+++ src/components/ExperienceData.js
@@ -1,16 +1,37 @@
 export const experienceData = [
     {
       companyName="Wayfair",
-      title="",
+      title="Software Engineer",
       companyIcon="",
       dateRange="February 2020 - present",
-      description=""
+      description=[]
     },
     {
-      companyName="",
-      title="",
+      companyName="Andersen Corporation (now PlyGem)",
+      title="Project Engineer",
       companyIcon="",
-      dateRange="",
-      description=""
+      dateRange="October 2017 - February 2019",
+      description=[]
+    },
+    {
+      companyName="Legrand",
+      title="Engineering Intern",
+      companyIcon="",
+      dateRange="Summer 2016",
+      description=[]
+    },
+    {
+      companyName="Cisco",
+      title="Extern",
+      companyIcon="",
+      dateRange="Winter 2016",
+      description=[]
+    },
+    {
+      companyName="Janssen Pharmaceuticals, of Johnson & Johnson",
+      title="Intern",
+      companyIcon="",
+      dateRange="Summer 2014, Summer 2015",
+      description=[]
     },
 ]
