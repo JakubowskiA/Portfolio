0afecc597f44d6d9ad3f7a483f9396bcf6fe8a8e
Ariel Jakubowski <arieljakubowski@gmail.com>
2020-12-12 11:40:20 -0500

    Added experience descriptions

--- src/components/ExperienceData.js
+++ src/components/ExperienceData.js
@@ -4,34 +4,34 @@ export const experienceData = [
       title="Software Engineer",
       companyIcon="",
       dateRange="February 2020 - present",
-      description=[]
+      description=["Designed and programmed new tool using React and PHP which enables managers to create their own training forms, reducing long term demand on Engineering teams", "Created database schema and technical and UI designs", "Wrote unit tests and participated in QA testing to ensure code passed all the quality benchmarks", "Resolved bugs and improved UI and information handling across multiple codebases"]
     },
     {
       companyName="Andersen Corporation (now PlyGem)",
       title="Project Engineer",
       companyIcon="",
       dateRange="October 2017 - February 2019",
-      description=[]
+      description=["Redesigned facilities using AutoCAD and Autodesk Inventor", "Investigated product defects and implemented corrective actions", "Standardized and improved manufacturing processes, reducing rejected product and process duration by 8%", "Managed $2M projects, including scope, schedule, and cost, saving $52M in expected loss", "Supervised contractors and technicians"]
     },
     {
       companyName="Legrand",
       title="Engineering Intern",
       companyIcon="",
       dateRange="Summer 2016",
-      description=[]
+      description=["Created Product Environmental Profiles", "Documented design changes to products and packaging and updated product CAD models"]
     },
     {
       companyName="Cisco",
       title="Extern",
       companyIcon="",
       dateRange="Winter 2016",
-      description=[]
+      description=["Shadowing program in Silicon Valley about innovation, start-ups, and tech giants like Google and Cisco"]
     },
     {
       companyName="Janssen Pharmaceuticals, of Johnson & Johnson",
       title="Intern",
       companyIcon="",
       dateRange="Summer 2014, Summer 2015",
-      description=[]
+      description=["Organized and maintained database", "Reconciled financial documentation and prepared financial reports for medical trials"]
     },
 ]
