3593163ea235e65c1990a2476a34fdb1fc4f7802
Ariel Jakubowski <arieljakubowski@gmail.com>
2020-12-20 10:27:31 -0500

    Reorganization

--- src/components/BlogPostsData.js
+++ src/components/BlogPostsData.js
@@ -1,23 +1,9 @@
 export const blogPostsData = [
   {
-    image: "https://miro.medium.com/max/800/1*9mhBwpoDiQSrmskvTtdsrg.jpeg",
-    title: "Breaking Down Object-Oriented Ruby",
-    publisher: "Published by Better Programming",
-    link:
-      "https://medium.com/better-programming/breaking-down-object-oriented-ruby-cb0a79e55a70",
-  },
-  {
     image: "https://miro.medium.com/max/700/0*M7CBNp4RBJRaZpJw.jpg",
     title: "An Introduction to SQL",
     publisher: "Published by The Startup",
     link: "https://medium.com/swlh/an-introduction-to-sql-ee45ee6f4ffc",
-  },
-  {
-    image: "https://miro.medium.com/max/700/1*32oIf2aGTs9U_oA21P5NdQ.png",
-    title: "The Universality of Empathetic Design",
-    publisher: "Published by UX Collective",
-    link:
-      "https://uxdesign.cc/the-universality-of-empathetic-design-bb6b562001c5",
   },
   {
     image: "https://miro.medium.com/max/700/1*oMha2rEoc4bbefw8KlDGbw.png",
@@ -25,26 +11,6 @@ export const blogPostsData = [
     publisher: "Published by Git Connected",
     link:
       "https://levelup.gitconnected.com/an-introduction-to-php-974101ed42d3",
-  },
-  {
-    image: "https://miro.medium.com/max/700/1*7fcDdBoJYPYNwSVa8FXGVg.png",
-    title: "Tips for Better Code Reviews",
-    publisher: "Published by Git Connected",
-    link:
-      "https://levelup.gitconnected.com/tips-for-better-code-reviews-2121fc338d3e",
-  },
-  {
-    image: "https://miro.medium.com/max/569/1*rGHEUeykJASszC9HgxsPxA.jpeg",
-    title: "Visual storytelling and UX design",
-    publisher: "Published by UX Collective",
-    link: "https://uxdesign.cc/visual-storytelling-and-ux-design-eebd394524d3",
-  },
-  {
-    image: "https://miro.medium.com/max/1000/1*Fuy7_WQaJKBsGb-MSRu17Q.png",
-    title: "An Introduction to Machine Learning",
-    publisher: "Published by Better Programming",
-    link:
-      "https://medium.com/better-programming/an-introduction-to-machine-learning-a4a6506c4bc7",
   },
   {
     image: "https://miro.medium.com/max/980/1*dgEOd9EmJ-wvmjhZ7fTFNw.png",
@@ -59,6 +25,40 @@ export const blogPostsData = [
     publisher: "Published by Data Driven Investor",
     link:
       "https://arieljakubowski.medium.com/solid-principles-explained-simply-eb9f94c5ea19",
+  },
+  {
+    image: "https://miro.medium.com/max/700/1*7fcDdBoJYPYNwSVa8FXGVg.png",
+    title: "Tips for Better Code Reviews",
+    publisher: "Published by Git Connected",
+    link:
+      "https://levelup.gitconnected.com/tips-for-better-code-reviews-2121fc338d3e",
+  },
+  {
+    image: "https://miro.medium.com/max/800/1*9mhBwpoDiQSrmskvTtdsrg.jpeg",
+    title: "Breaking Down Object-Oriented Ruby",
+    publisher: "Published by Better Programming",
+    link:
+      "https://medium.com/better-programming/breaking-down-object-oriented-ruby-cb0a79e55a70",
+  },
+  {
+    image: "https://miro.medium.com/max/700/1*32oIf2aGTs9U_oA21P5NdQ.png",
+    title: "The Universality of Empathetic Design",
+    publisher: "Published by UX Collective",
+    link:
+      "https://uxdesign.cc/the-universality-of-empathetic-design-bb6b562001c5",
+  },
+  {
+    image: "https://miro.medium.com/max/569/1*rGHEUeykJASszC9HgxsPxA.jpeg",
+    title: "Visual storytelling and UX design",
+    publisher: "Published by UX Collective",
+    link: "https://uxdesign.cc/visual-storytelling-and-ux-design-eebd394524d3",
+  },
+  {
+    image: "https://miro.medium.com/max/1000/1*Fuy7_WQaJKBsGb-MSRu17Q.png",
+    title: "An Introduction to Machine Learning",
+    publisher: "Published by Better Programming",
+    link:
+      "https://medium.com/better-programming/an-introduction-to-machine-learning-a4a6506c4bc7",
   },
   {
     image: "https://miro.medium.com/max/676/1*H6AJcRKWwzS_-c-zRaR0bQ.jpeg",
