c6f11e47aa9285a3d9b05e437649c78519ee134d
Ariel Jakubowski <arieljakubowski@gmail.com>
2020-12-24 15:53:09 -0500

    Format

--- src/App.js
+++ src/App.js
@@ -1,53 +1,49 @@
-import React, { Fragment } from 'react';
-import './styles/style.css';
+import React, { Fragment } from "react";
+import "./styles/style.css";
 // import { Link } from "react-router-dom";
 // import { Route, withRouter } from 'react-router-dom';
-import AboutMe from './components/AboutMe';
-import Resume from './components/Resume';
-import Experience from './components/Experience'
-import Projects from './components/Projects';
-import Blog from './components/Blog';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
+import AboutMe from "./components/AboutMe";
+import Resume from "./components/Resume";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Blog from "./components/Blog";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
 
 // import './App.css';
 
 function App() {
   return (
     <Fragment>
-    <div className="header"></div>
-    <div className="navbar">
-    <div className="nav-name">
-        Ariel Jakubowski
+      <div className="header"></div>
+      <div className="navbar">
+        <div className="nav-name">Ariel Jakubowski</div>
+        <div className="nav-item hvr-grow">
+          <a href="#about-me">About Me</a>
+        </div>
+        <div className="nav-item hvr-grow">
+          <a href="#experience">Experience</a>
+        </div>
+        <div className="nav-item hvr-grow">
+          <a href="#projects">Projects</a>
+        </div>
+        <div className="nav-item hvr-grow">
+          <a href="#blog">Blog</a>
+        </div>
+        <div className="nav-item hvr-grow">
+          <a href="#skills">Skills</a>
+        </div>
+        <div className="nav-item hvr-grow">
+          <a href="#contact">Contact</a>
+        </div>
       </div>
-      <div className="nav-item hvr-grow">
-        <a href="#about-me">About Me</a>
-      </div>
-      <div className="nav-item hvr-grow">
-        <a href="#experience">Experience</a>
-      </div>
-      <div className="nav-item hvr-grow">
-      <a href="#projects">Projects</a>
-      </div>
-      <div className="nav-item hvr-grow">
-        <a href="#blog">Blog</a>
-      </div>
-      <div className="nav-item hvr-grow">
-      <a href="#skills">Skills</a>
-      </div>
-      <div className="nav-item hvr-grow">
-      <a href="#contact">Contact</a>
-      </div>
-
-    </div>
-    <AboutMe />
-    <Experience/>
-    <Projects />
-    {/* <Resume/> */}
-    <Blog />
-    <Skills/>
-    <Contact/>
-    
+      <AboutMe />
+      <Experience />
+      <Projects />
+      {/* <Resume/> */}
+      <Blog />
+      <Skills />
+      <Contact />
     </Fragment>
   );
 }
